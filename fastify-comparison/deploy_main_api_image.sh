fa6f049706b911a9eebb624aa825cec16d938087
alex <buralex89@gmail.com>
2023-09-22 22:25:35 +0300

    fix: image names

--- fastify-comparison/deploy_main_api_image.sh
+++ fastify-comparison/deploy_main_api_image.sh
@@ -3,8 +3,6 @@
 [ -z "$PROJECT_NAME" ] && echo "Empty PROJECT_NAME" && exit 1
 [ -z "$DEPLOY_ENV" ] && echo "Empty DEPLOY_ENV" && exit 1
 
-MAIN_API_IMAGE_NAME=buralex/public:$PROJECT_NAME-main-api
-
 echo "image name: $MAIN_API_IMAGE_NAME"
 
 docker build -t $MAIN_API_IMAGE_NAME .
