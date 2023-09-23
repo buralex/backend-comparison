1fa9a98b4da08c38f1047d12302ac65cd8706d21
alex <buralex89@gmail.com>
2023-09-23 15:23:00 +0300

    aaa

---
+++ rust-comparison/deploy_main_api_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ -z "$PROJECT_NAME" ] && echo "Empty PROJECT_NAME" && exit 1
+[ -z "$DEPLOY_ENV" ] && echo "Empty DEPLOY_ENV" && exit 1
+
+echo "image name: $MAIN_API_IMAGE_NAME"
+
+docker build -t $MAIN_API_IMAGE_NAME .
+docker push $MAIN_API_IMAGE_NAME
+
+if [ $? -eq 0 ]; then
+    echo "Docker build and push command executed successfully."
+else
+    echo "Error: Docker build and push command failed."
+    exit 1
+fi
