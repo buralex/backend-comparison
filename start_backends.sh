b2d877518bd2e9df9caf129b61d5f40079174c0b
alex <buralex89@gmail.com>
2023-09-23 15:22:57 +0300

    aaa

--- start_backends.sh
+++ start_backends.sh
@@ -47,3 +47,20 @@ docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull al
 check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
 
 curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+
+# ============================================
+#                  rust
+# ============================================
+# cd ..
+cd ~/backend-comparison/rust-comparison
+cp .env.example .env.prod
+sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
+sed -i.bak "s/POSTGRES_HOST=.*/POSTGRES_HOST=postgres-db/" .env.prod
+sed -i.bak "s/POSTGRES_PORT=.*/POSTGRES_PORT=5432/" .env.prod
+set -o allexport && . ./.env.prod && set +o allexport
+
+docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
+
+# check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
+
+# curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
