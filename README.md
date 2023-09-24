ebbfb4e0099d13936b6a32b5623136b363be8110
alex <buralex89@gmail.com>
2023-09-25 02:58:26 +0300

    feat: update readme

--- README.md
+++ README.md
@@ -9,23 +9,27 @@ Just a boilerplate for starting backends on different languages for comparison.
    Start all backend services (in docker)
 
    ```bash
-   sh start_backends.sh
+   sh start_backends.sh --nodejs true --go true --rust true
    ```
 
 2. Now you can access apis:
    | Name | Root url |
    |------------|--------------|
-   | Fastify | http://localhost:3028 |
-   | Gin | http://localhost:3029 |
+   | Node.js (Fastify) | http://localhost:3028 |
+   | Go (Gin) | http://localhost:3029 |
+   | Rust (Actix Web) | http://localhost:3030 |
 
    Common routes for all apis:
    | Route | description |
    |------------|--------------|
    | /ping | Returns string "pong" |
    | /users | Returns list of users with posts |
+   | /fib/:number | Calculates fibonacci for a given number |
 
 ### Commands for benchmarks:
 
+```bash
 wrk -t4 -c100 -d10s http://localhost:3028/users
-
 wrk -t4 -c100 -d10s http://localhost:3029/users
+wrk -t4 -c100 -d10s http://localhost:3030/users
+```
--- start_backends.sh
+++ start_backends.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Parsing passed arguments (example: sh some_script.sh --somearg true)
+# https://unix.stackexchange.com/a/388038
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+   fi
+  shift
+done
+
 check_app_status() {
   local app_url=$1
 
@@ -15,50 +25,47 @@ check_app_status() {
   done
 }
 
-# ============================================
-#                  fastify
-# ============================================
-cd ~/backend-comparison/nodejs-comparison
-cp .env.example .env.prod
-sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
-sed -i.bak "s/POSTGRES_HOST=.*/POSTGRES_HOST=postgres-db/" .env.prod
-sed -i.bak "s/POSTGRES_PORT=.*/POSTGRES_PORT=5432/" .env.prod
-set -o allexport && . ./.env.prod && set +o allexport
+if [ "$nodejs" == "true" ]; then
+  cd ~/backend-comparison/nodejs-comparison
+  cp .env.example .env.prod
+  sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
+  sed -i.bak "s/POSTGRES_HOST=.*/POSTGRES_HOST=postgres-db/" .env.prod
+  sed -i.bak "s/POSTGRES_PORT=.*/POSTGRES_PORT=5432/" .env.prod
+  set -o allexport && . ./.env.prod && set +o allexport
 
-docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
+  docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
 
-check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
+  check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
 
-curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+  curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+fi
 
-# ============================================
-#                  gin
-# ============================================
-cd ~/backend-comparison/go-comparison
-cp .env.example .env.prod
-sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
-sed -i.bak "s/POSTGRES_HOST=.*/POSTGRES_HOST=postgres-db/" .env.prod
-sed -i.bak "s/POSTGRES_PORT=.*/POSTGRES_PORT=5432/" .env.prod
-set -o allexport && . ./.env.prod && set +o allexport
+if [ "$go" == "true" ]; then
+  cd ~/backend-comparison/go-comparison
+  cp .env.example .env.prod
+  sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
+  sed -i.bak "s/POSTGRES_HOST=.*/POSTGRES_HOST=postgres-db/" .env.prod
+  sed -i.bak "s/POSTGRES_PORT=.*/POSTGRES_PORT=5432/" .env.prod
+  set -o allexport && . ./.env.prod && set +o allexport
 
-docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
+  docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
 
-check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
+  check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
 
-curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+  curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+fi
 
-# ============================================
-#                  rust
-# ============================================
-cd ~/backend-comparison/rust-comparison
-cp .env.example .env.prod
-sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
-sed -i.bak "s/PG__HOST=.*/PG__HOST=postgres-db/" .env.prod
-sed -i.bak "s/PG__PORT=.*/PG__PORT=5432/" .env.prod
-set -o allexport && . ./.env.prod && set +o allexport
+if [ "$rust" == "true" ]; then
+  cd ~/backend-comparison/rust-comparison
+  cp .env.example .env.prod
+  sed -i.bak "s/DEPLOY_ENV=.*/DEPLOY_ENV=prod/" .env.prod
+  sed -i.bak "s/PG__HOST=.*/PG__HOST=postgres-db/" .env.prod
+  sed -i.bak "s/PG__PORT=.*/PG__PORT=5432/" .env.prod
+  set -o allexport && . ./.env.prod && set +o allexport
 
-docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
+  docker compose -f compose.yml --project-name $PROJECT_NAME up --detach --pull always
 
-check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
+  check_app_status "http://localhost:$MAIN_API_SERVICE_PORT/ping"
 
-curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+  curl http://localhost:$MAIN_API_SERVICE_PORT/helpers/seed
+fi
