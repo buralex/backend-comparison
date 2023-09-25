dc9f96efd1b1d66df58afaebc2bed58a5bda4bf6
alex <buralex89@gmail.com>
2023-09-25 03:03:02 +0300

    chore: remove unnecessary comments

--- remove_backends.sh
+++ remove_backends.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# ============================================
-#                  fastify
-# ============================================
 cd ~/backend-comparison/nodejs-comparison
 set -o allexport && . ./.env.prod && set +o allexport
 docker compose -f compose.yml --project-name $PROJECT_NAME down --volumes
 
-# ============================================
-#                  gin
-# ============================================
 cd ~/backend-comparison/go-comparison
 set -o allexport && . ./.env.prod && set +o allexport
 docker compose -f compose.yml --project-name $PROJECT_NAME down --volumes
 
-# ============================================
-#                  rust
-# ============================================
 cd ~/backend-comparison/rust-comparison
 set -o allexport && . ./.env.prod && set +o allexport
 docker compose -f compose.yml --project-name $PROJECT_NAME down --volumes
 
-# ============================================
-#                  common
-# ============================================
-  echo "### Cleaning unused images"
-  docker image prune --all --force
+echo "### Cleaning unused images"
+docker image prune --all --force
